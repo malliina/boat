17f6bbdf04d9a664be2beac546f3dd97e5991302
Michael Skogberg <michael.skogberg@futurice.com>
2019-07-02 23:03:40 +0300

    Fix vendor js

--- frontend/src/main/resources/vendors/vendors.js
+++ frontend/src/main/resources/vendors/vendors.js
@@ -1,4 +1,4 @@
-import '../../../../target/scala-2.12/scalajs-bundler/main/node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../../../../target/scala-2.12/scalajs-bundler/main/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-import '../../../../target/scala-2.12/scalajs-bundler/main/node_modules/mapbox-gl/dist/mapbox-gl.css'
-import '../../../../target/scala-2.12/scalajs-bundler/main/node_modules/@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import '../../../../target/scala-2.13/scalajs-bundler/main/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../../../target/scala-2.13/scalajs-bundler/main/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '../../../../target/scala-2.13/scalajs-bundler/main/node_modules/mapbox-gl/dist/mapbox-gl.css'
+import '../../../../target/scala-2.13/scalajs-bundler/main/node_modules/@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
