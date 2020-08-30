c708882b8ddf6b72a72f70ed1f0cbbb24622fd1e
Michael Skogberg <malliina123@gmail.com>
2020-08-30 20:21:43 +0300

    Implement user management in doobie

--- backend/app/com/malliina/boat/db/DoobieTracksDatabase.scala
+++ backend/app/com/malliina/boat/db/DoobieTracksDatabase.scala
@@ -11,33 +11,27 @@ import doobie.implicits._
 import scala.concurrent.Future
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
-object DoobieTracksDatabase {
-  def apply(db: DoobieDatabase): DoobieTracksDatabase = new DoobieTracksDatabase(db)
-}
+object CommonSql extends CommonSql
 
-class DoobieTracksDatabase(val db: DoobieDatabase) extends TracksSource with StatsSource {
-  implicit val ec = db.ec
-  import DoobieMappings._
-
-  object sql {
-    val boats =
-      sql"""select b.id, b.name, b.token, u.id uid, u.user, u.email, u.language
+trait CommonSql {
+  val boats =
+    sql"""select b.id, b.name, b.token, u.id uid, u.user, u.email, u.language
          from boats b, users u 
          where b.owner = u.id"""
-    val topPoints =
-      sql"""select winners.track track, min(winners.id) point
+  val topPoints =
+    sql"""select winners.track track, min(winners.id) point
           from (select p.id, p.track
                 from points p,
                     (select track, max(boat_speed) maxSpeed from points group by track) tops
                 where p.track = tops.track and p.boat_speed = tops.maxSpeed) winners
           group by winners.track"""
-    val selectAllPoints =
-      sql"""select id, longitude, latitude, coord, boat_speed, water_temp, depthm, depth_offsetm, boat_time, track, track_index, diff, added 
+  val selectAllPoints =
+    sql"""select id, longitude, latitude, coord, boat_speed, water_temp, depthm, depth_offsetm, boat_time, track, track_index, diff, added 
            from points p"""
-    val topRows =
-      sql"""$selectAllPoints where p.id in (select point from ($topPoints) fastestPoints)"""
-    val minMaxTimes =
-      sql"""select track,
+  val topRows =
+    sql"""$selectAllPoints where p.id in (select point from ($topPoints) fastestPoints)"""
+  val minMaxTimes =
+    sql"""select track,
        min(boat_time)                                        start,
        max(boat_time)                                        end,
        timestampdiff(SECOND, min(boat_time), max(boat_time)) secs,
@@ -46,21 +40,32 @@ class DoobieTracksDatabase(val db: DoobieDatabase) extends TracksSource with Sta
        year(min(boat_time))                                  startYear
         from points p
         group by track"""
-    val timedTracks =
-      sql"""select t.id, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, times.secs secs, times.start, times.end, times.startDate, times.startMonth, times.startYear, t.boat
+  val timedTracks =
+    sql"""select t.id, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, times.secs secs, times.start, times.end, times.startDate, times.startMonth, times.startYear, t.boat
            from tracks t,
            ($minMaxTimes) times 
            where t.id = times.track"""
-    val trackHighlights =
-      sql"""select t.id, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.end, t.secs, t.startDate, t.startMonth, t.startYear, t.boat, top.id pointId, top.longitude, top.latitude, top.coord, top.boat_speed, top.water_temp, top.depthm, top.depth_offsetm, top.boat_time, date(top.boat_time) trackDate, top.track, top.added topAdded
+  val trackHighlights =
+    sql"""select t.id, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.end, t.secs, t.startDate, t.startMonth, t.startYear, t.boat, top.id pointId, top.longitude, top.latitude, top.coord, top.boat_speed, top.water_temp, top.depthm, top.depth_offsetm, top.boat_time, date(top.boat_time) trackDate, top.track, top.added topAdded
             from ($topRows) top, ($timedTracks) t
             where top.track = t.id"""
-    val trackColumns =
-      fr"t.id tid, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.startDate, t.startMonth, t.startYear, t.end, t.secs duration, t.boat_speed maxBoatspeed, t.pointId, t.longitude, t.latitude, t.coord, t.boat_speed topSpeed, t.water_temp, t.depthm, t.depth_offsetm, t.boat_time, t.trackDate, t.track, t.topAdded, b.id boatId, b.name boatName, b.token, b.uid, b.user, b.email, b.language"
-    val nonEmptyTracks =
-      sql"""select t.id tid, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.startDate, t.startMonth, t.startYear, t.end, t.secs duration, t.boat_speed maxBoatspeed, t.pointId, t.longitude, t.latitude, t.coord, t.boat_speed topSpeed, t.water_temp, t.depthm, t.depth_offsetm, t.boat_time, t.trackDate, t.track, t.topAdded, b.id boatId, b.name boatName, b.token, b.uid, b.user, b.email, b.language
+  val trackColumns =
+    fr"t.id tid, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.startDate, t.startMonth, t.startYear, t.end, t.secs duration, t.boat_speed maxBoatspeed, t.pointId, t.longitude, t.latitude, t.coord, t.boat_speed topSpeed, t.water_temp, t.depthm, t.depth_offsetm, t.boat_time, t.trackDate, t.track, t.topAdded, b.id boatId, b.name boatName, b.token, b.uid, b.user, b.email, b.language"
+  val nonEmptyTracks =
+    sql"""select t.id tid, t.name, t.title, t.canonical, t.comments, t.added, t.points, t.avg_speed, t.avg_water_temp, t.distance, t.start, t.startDate, t.startMonth, t.startYear, t.end, t.secs duration, t.boat_speed maxBoatSpeed, t.pointId, t.longitude, t.latitude, t.coord, t.boat_speed topSpeed, t.water_temp, t.depthm, t.depth_offsetm, t.boat_time, t.trackDate, t.track, t.topAdded, b.id boatId, b.name boatName, b.token, b.uid, b.user owner, b.email, b.language
           from ($boats) b, ($trackHighlights) t
           where b.id = t.boat"""
+}
+
+object DoobieTracksDatabase {
+  def apply(db: DoobieDatabase): DoobieTracksDatabase = new DoobieTracksDatabase(db)
+}
+
+class DoobieTracksDatabase(val db: DoobieDatabase) extends TracksSource with StatsSource {
+  implicit val ec = db.ec
+  import DoobieMappings._
+
+  object sql extends CommonSql {
     val pointColumns =
       fr"p.id, p.longitude, p.latitude, p.coord, p.boat_speed, p.water_temp, p.depthm, p.depth_offsetm, p.boat_time, date(p.boat_time), p.track, p.added"
     def pointsByTrack(name: TrackName) =
--- backend/app/com/malliina/boat/db/DoobieUserManager.scala
+++ backend/app/com/malliina/boat/db/DoobieUserManager.scala
@@ -1,37 +1,208 @@
 package com.malliina.boat.db
 
-import com.malliina.values.{Email, UserId}
+import com.malliina.boat.db.DoobieMappings._
+import com.malliina.boat.http.{AccessResult, InviteInfo}
+import com.malliina.boat.{BoatInfo, BoatNames, BoatToken, BoatTokens, DeviceId, InviteState, JoinedBoat, JoinedTrack, Language, TimeFormatter, UserBoats, UserInfo, UserToken, Usernames}
+import com.malliina.values.{Email, UserId, Username}
+import doobie._
 import doobie.implicits._
-import DoobieMappings._
-import com.malliina.boat.{BoatToken, JoinedBoat}
+import play.api.Logger
 
 import scala.concurrent.Future
 
 class DoobieUserManager(db: DoobieDatabase) {
-  val selectUsers = sql"select id, user, email, token, language, enabled, added from users u"
+  object sql extends CommonSql
+  private val log = Logger(getClass)
+
+  val userColumns = fr"u.id, u.user, u.email, u.token, u.language, u.enabled, u.added"
+  val selectUsers = sql"select $userColumns from users u"
+
+  def userInsertion(user: NewUser) =
+    sql"""insert into users(user, email, token, enabled)
+          values(${user.user}, ${user.email}, ${user.token}, ${user.enabled})""".update
+      .withUniqueGeneratedKeys[UserId]("id")
   def userById(id: UserId) = sql"$selectUsers where u.id = $id"
   def userByEmail(email: Email) = sql"$selectUsers where u.email = $email"
-
+  def userByName(name: Username) = sql"$selectUsers where u.user = $name"
+  def userByEmailIO(email: Email) = userByEmail(email).query[UserRow].unique
   def userMeta(email: Email): Future[UserRow] = db.run {
-    userByEmail(email).query[UserRow].unique
+    userByEmailIO(email)
+  }
+
+  def userInfo(email: Email): Future[UserInfo] = db.run {
+    def by(id: UserId) =
+      sql"""select $userColumns, b.id boatId, b.name boatName, b.token boatToken, b.owner boatOwner, b.added boatAdded
+            from users u
+            left join boats b on b.owner = u.id
+            where u.id = $id""".query[JoinedUser].to[List]
+    val task = for {
+      userId <- getOrCreate(email)
+      info <- by(userId).map(NewUserManager.collect)
+    } yield info
+    task.flatMap[UserInfo] { infos =>
+      infos.headOption.map { profile =>
+        // Type annotation helps here for some reason
+        val checked: ConnectionIO[UserInfo] =
+          if (profile.enabled) pure(profile)
+          else fail(IdentityException(UserDisabled(profile.username)))
+        checked
+      }.getOrElse {
+        fail(IdentityException(InvalidCredentials(None)))
+      }
+    }
   }
 
   def authBoat(token: BoatToken): Future[JoinedBoat] = db.run {
-    sql"""select b.id, b.name, b.token, u.id uid, u.user, u.email, u.language 
-          from boats b, users u 
-          where b.owner = u.id and b.token = $token"""
-      .query[JoinedBoat]
-      .unique
+    val boats = CommonSql.boats
+    sql"$boats and b.token = $token".query[JoinedBoat].option.flatMap { opt =>
+      opt.map { b => pure(b) }.getOrElse { fail(IdentityException(InvalidToken(token))) }
+    }
+  }
+
+  def boats(email: Email) = db.run {
+    def boatRowsIO(id: UserId) = sql"""${sql.nonEmptyTracks} and owner = $id and t.points > 100"""
+      .query[JoinedTrack]
+      .to[List]
+    def deviceRowsIO(email: Email) =
+      sql"""${sql.boats} and email = $email and id not in (select boat from tracks)"""
+        .query[JoinedBoat]
+        .to[List]
+    for {
+      id <- getOrCreate(email)
+      user <- userById(id).query[UserRow].unique
+      boatRows <- boatRowsIO(id)
+      devices <- deviceRowsIO(email)
+    } yield {
+      val bs = NewUserManager.collectBoats(boatRows, TimeFormatter(user.language))
+      val gpsDevices = devices.map(d => BoatInfo(d.device, d.boatName, d.username, d.language, Nil))
+      UserBoats(user.user, user.language, bs ++ gpsDevices)
+    }
+  }
+
+  def initUser(user: Username = Usernames.anon): Future[NewUser] = db.run {
+    val anon = NewUser(user, None, UserToken.random(), enabled = true)
+    for {
+      exists <- userByName(user).query[UserRow].option
+      ok <- exists.map(u => pure(u.user)).getOrElse(userInsertion(anon))
+    } yield anon
   }
 
   def addUser(user: NewUser): Future[Either[AlreadyExists, UserRow]] = db.run {
-    val insertion =
-      sql"""insert into users(user, email, token, enabled) 
-            values(${user.user}, ${user.email}, ${user.token}, ${user.enabled})""".update
-        .withUniqueGeneratedKeys[UserId]("id")
-    for {
-      id <- insertion
-      row <- userById(id).query[UserRow].unique
-    } yield Right(row)
+    userInsertion(user).flatMap { uid =>
+      userById(uid).query[UserRow].unique
+    }.map[Either[AlreadyExists, UserRow]] { u =>
+      Right(u)
+    }.exceptSql { sqle =>
+      if (sqle.getMessage.contains("primary key violation"))
+        pure(Left(AlreadyExists(user.user)))
+      else
+        fail(sqle)
+    }
   }
+
+  def deleteUser(user: Username): Future[Either[UserDoesNotExist, Unit]] = db.run {
+    sql"""delete from users where user = $user""".update.run.map { changed =>
+      if (changed > 0) {
+        log.info(s"Deleted user '$user'.")
+        Right(())
+      } else {
+        Left(UserDoesNotExist(user))
+      }
+    }
+  }
+
+  def changeLanguage(user: UserId, to: Language): Future[Boolean] = db.run {
+    sql"""update users set language = $to where id = $user""".update.run.map { changed =>
+      val wasChanged = changed > 0
+      if (wasChanged) {
+        log.info(s"Changed language of user ID '$user' to '$to'.")
+      }
+      wasChanged
+    }
+  }
+
+  def userBoat(boat: DeviceId, user: UserId) =
+    sql"""select user, boat, state, added
+         from users_boats 
+         where user = $user and boat = $boat"""
+
+  def invite(i: InviteInfo): Future[AccessResult] = db.run {
+    userByEmail(i.email).query[UserRow].option.flatMap { invitee =>
+      invitee.map { user =>
+        addInviteIO(i.boat, user.id, i.principal)
+      }.getOrElse {
+        pure(AccessResult(false))
+      }
+    }
+  }
+
+  private def linkInvite(boat: DeviceId, to: UserId) =
+    sql"""insert into users_boats(user, boat, state) values($to, $boat, ${InviteState.Awaiting.name})"""
+
+  private def getOrCreate(email: Email) = for {
+    existing <- userByEmail(email).query[UserRow].option
+    userId <- existing.map(u => pure(u.id)).getOrElse(addUserWithBoat(email))
+  } yield userId
+
+  private def addUserWithBoat(email: Email) = {
+    def boatInsertion(owner: UserId) =
+      sql"""insert into boats(name, token, owner)
+           values(${BoatNames.random()}, ${BoatTokens.random()}, $owner)""".update.run
+    for {
+      userId <- userInsertion(NewUser.email(email))
+      _ <- boatInsertion(userId)
+    } yield userId
+  }
+  def grantAccess(boat: DeviceId, to: UserId, principal: UserId): Future[AccessResult] = db.run {
+    addInviteIO(boat, to, principal)
+  }
+
+  def revokeAccess(boat: DeviceId, from: UserId, principal: UserId): Future[AccessResult] = db.run {
+    manageGroups(boat, from, principal) { existed =>
+      if (existed) sql"delete from users_boats where boat = $boat and user = $from".update.run
+      else pure(0)
+    }
+  }
+
+  def updateInvite(boat: DeviceId, user: UserId, state: InviteState): Future[Long] = db.run {
+    sql"update users_boats set state = $state where boat = $boat and user = $user".update.run
+  }
+
+  private def addInviteIO(boat: DeviceId, to: UserId, principal: UserId) =
+    manageGroups(boat, to, principal) { existed =>
+      if (existed) {
+        pure(0)
+      } else {
+        linkInvite(boat, to).update.run
+      }
+    }
+
+  private def manageGroups[T](boat: DeviceId, from: UserId, principal: UserId)(
+    run: Boolean => ConnectionIO[T]
+  ): ConnectionIO[AccessResult] = {
+    for {
+      owns <-
+        sql"""select b.id from boats b where b.id = $boat and b.owner = $principal"""
+          .query[DeviceId]
+          .option
+      boatId <-
+        owns
+          .map(pure)
+          .getOrElse(
+            fail(
+              s"User $principal is not authorized to modify access to boat $boat for user $from."
+            )
+          )
+      link <-
+        sql"select ub.user from users_boats ub where ub.boat = $boat and ub.user = $from"
+          .query[UserId]
+          .option
+      res <- run(link.isDefined).map(t => AccessResult(link.isDefined))
+    } yield res
+  }
+
+  protected def pure[A](a: A): ConnectionIO[A] = AsyncConnectionIO.pure(a)
+  protected def fail[A](message: String): ConnectionIO[A] = fail(new Exception(message))
+  protected def fail[A](e: Throwable): ConnectionIO[A] = AsyncConnectionIO.raiseError(e)
+
 }
