53cdf3c831f5d1aabac969f8a7fc56e0f1ea6918
Michael Skogberg <michael.skogberg@futurice.com>
2023-04-10 17:13:43 +0300

    Fix sql

--- backend/src/main/resources/db/migration/V9__cars_notifications.sql
+++ backend/src/main/resources/db/migration/V9__cars_notifications.sql
@@ -4,4 +4,6 @@ create table push_history
   device bigint                                    not null,
   added  timestamp(3) default CURRENT_TIMESTAMP(3) not null,
   constraint push_history_device_fk foreign key (device) references boats (id) on update cascade on delete cascade
-) charset = utf8mb4
+) charset = utf8mb4;
+
+create index push_history_device_idx on push_history(device, added);
