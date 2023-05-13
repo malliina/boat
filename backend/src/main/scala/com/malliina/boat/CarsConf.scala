847901debdd792534f9f9938d761357835b33861
Michael Skogberg <michael.skogberg@futurice.com>
2023-05-13 21:41:31 +0300

    Add car langs

--- backend/src/main/scala/com/malliina/boat/CarsConf.scala
+++ backend/src/main/scala/com/malliina/boat/CarsConf.scala
@@ -7,16 +7,42 @@ case class CarProfileLang(
   driving: String,
   chooseLanguage: String,
   signInWith: String,
-  signOut: String
+  signOut: String,
+  failedToLoadProfile: String,
+  failedToSignIn: String,
+  goToMap: String,
+  version: String,
+  nothingHere: String
 ) derives Codec.AsObject
-case class CarStatsLang(speed: String, height: String, nightMode: String, dayMode: String)
+case class CarStatsLang(
+  speed: String,
+  height: String,
+  nightMode: String,
+  dayMode: String,
+  bearing: String,
+  accuracy: String,
+  degrees: String,
+  meters: String,
+  batteryLevel: String,
+  capacity: String,
+  range: String,
+  outsideTemperature: String
+) derives Codec.AsObject
+case class PermissionsLang(
+  grantCta: String,
+  grantAccess: String,
+  explanation: String,
+  tryAgain: String,
+  openSettingsText: String
+) derives Codec.AsObject
+case class CarSettingsLang(title: String, openSettings: String, selectCar: String, noCars: String)
   derives Codec.AsObject
-case class CarSettingsLang(selectCar: String) derives Codec.AsObject
 case class CarLang(
   appName: String,
   language: Language,
   profile: CarProfileLang,
   settings: CarSettingsLang,
+  permissions: PermissionsLang,
   stats: CarStatsLang
 ) derives Codec.AsObject
 case class CarLanguages(finnish: CarLang, swedish: CarLang, english: CarLang) derives Codec.AsObject
@@ -30,24 +56,111 @@ object CarLanguages:
         "Ajat autoa",
         "Valitse kieli",
         "Kirjaudu palvelulla",
-        "Kirjaudu ulos"
+        "Kirjaudu ulos",
+        "Käyttäjätietojen lataus epäonnistui.",
+        "Kirjautuminen epäonnistui.",
+        "Kartta",
+        "Version",
+        "Täällä ei ole mitään."
       ),
-      CarSettingsLang("Valitse auto"),
-      CarStatsLang("Nopeus", "Korkeus", "Yötila", "Päivätila")
+      CarSettingsLang("Asetukset", "Avaa asetukset", "Valitse auto", "Ei autoja."),
+      PermissionsLang(
+        "Anna oikeuksia",
+        "Anna sovellukselle oikeudet lukea auton sijainti ja muut tiedot",
+        "Sovellus tarvitsee oikeuksia tallentaakseen sijainti- ja muut tiedot Car-Tracker-tilillesi.",
+        "Kokeile uudelleen",
+        "Avaa asetukset ja anna sovellukselle oikeuksia."
+      ),
+      CarStatsLang(
+        "Nopeus",
+        "Korkeus",
+        "Yötila",
+        "Päivätila",
+        "Suunta",
+        "Tarkkuus",
+        "astetta",
+        "metriä",
+        "Akun varaus",
+        "Kapasiteetti",
+        "Toimintamatka",
+        "Ulkolämpötila"
+      )
     ),
     CarLang(
       "Car-Tracker",
       Language.swedish,
-      CarProfileLang("Inloggad som", "Du kör", "Välj språk", "Logga in med", "Logga ut"),
-      CarSettingsLang("Välj bil"),
-      CarStatsLang("Hastighet", "Höjd", "Nattläge", "Dagläge")
+      CarProfileLang(
+        "Inloggad som",
+        "Du kör",
+        "Välj språk",
+        "Logga in med",
+        "Logga ut",
+        "Laddning av profil misslyckades.",
+        "Inloggning misslyckades.",
+        "Karta",
+        "Version",
+        "Ingenting här."
+      ),
+      CarSettingsLang("Inställningar", "Öppna inställningar", "Välj bil", "Inga bilar."),
+      PermissionsLang(
+        "Ge rättigheter",
+        "Ge appen rättigheter till plats och bildata",
+        "Appen behöver rättigheter för att kunna spara plats och bildata till ditt Car-Tracker konto.",
+        "Pröva på nytt",
+        "Öppna inställningar och ge appen rättigheter."
+      ),
+      CarStatsLang(
+        "Hastighet",
+        "Höjd",
+        "Nattläge",
+        "Dagläge",
+        "Bearing",
+        "Accuracy",
+        "degrees",
+        "meters",
+        "Batterinivå",
+        "Kapacitet",
+        "Räckvidd",
+        "Temperatur ute"
+      )
     ),
     CarLang(
       "Car-Tracker",
       Language.english,
-      CarProfileLang("Signed in as", "Driving", "Select language", "Sign in with", "Sign out"),
-      CarSettingsLang("Select car"),
-      CarStatsLang("Speed", "Height", "Night mode", "Day mode")
+      CarProfileLang(
+        "Signed in as",
+        "Driving",
+        "Select language",
+        "Sign in with",
+        "Sign out",
+        "Failed to load profile.",
+        "Failed to sign in.",
+        "Go to map",
+        "Version",
+        "Nothing to see here."
+      ),
+      CarSettingsLang("Settings", "Open Settings", "Select car", "No cars."),
+      PermissionsLang(
+        "Grant permissions",
+        "Grant app access to location and car",
+        "This app needs access to location and car properties in order to store them to your Car-Tracker account.",
+        "Try again",
+        "Please open Settings and grant app-level permissions for this app."
+      ),
+      CarStatsLang(
+        "Speed",
+        "Height",
+        "Night mode",
+        "Day mode",
+        "Bearing",
+        "Accuracy",
+        "degrees",
+        "meters",
+        "Battery level",
+        "Capacity",
+        "Range",
+        "Temperature outside"
+      )
     )
   )
 
