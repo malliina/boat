f76ed1826d77c51491dd84d36ae8df8e6b8d0073
Michael Skogberg <michael.skogberg@futurice.com>
2023-10-28 21:28:02 +0300

    Fixes

--- shared/shared/src/test/scala/com/malliina/boat/ModelTests.scala
+++ shared/shared/src/test/scala/com/malliina/boat/ModelTests.scala
@@ -3,25 +3,23 @@ package com.malliina.boat
 import com.malliina.values.{lng, lat}
 import io.circe.{Codec, Printer}
 import io.circe.generic.semiauto.deriveCodec
-import io.circe.parser.decode
+import io.circe.parser.{decode, parse}
 import io.circe.syntax.EncoderOps
 import concurrent.duration.DurationInt
 
 class ModelTests extends munit.FunSuite:
-  test("coord cheap hash") {
+  test("coord cheap hash"):
     val c = Coord(12.1.lng, 13.412456789.lat)
     assertEquals(c.approx, "12.10000,13.41245")
-  }
 
-  test("do not serialize None as null") {
+  test("do not serialize None as null"):
     case class MyClass(name: String, age: Option[Int])
     implicit val codec: Codec[MyClass] = deriveCodec[MyClass]
     val printer = Printer.noSpaces.copy(dropNullValues = true)
     val str = MyClass("Santa", None).asJson.printWith(printer)
     assertEquals(str, """{"name":"Santa"}""")
-  }
 
-  test("parse vessel location") {
+  test("parse vessel location"):
     val in =
       """
         |{
@@ -47,9 +45,8 @@ class ModelTests extends munit.FunSuite:
     val result = decode[VesselLocation](in)(VesselLocation.readerGeoJson)
     // println(result)
     assert(result.isRight)
-  }
 
-  test("parse vessel metadata") {
+  test("parse vessel metadata"):
     val in =
       """
         |{
@@ -73,9 +70,100 @@ class ModelTests extends munit.FunSuite:
     val result = decode[VesselMetadata](in)(VesselMetadata.readerGeoJson)
     // println(result)
     assert(result.isRight)
-  }
 
-  test("PingEvent JSON") {
-    val string = PingEvent(123, 1.seconds).asJson.noSpaces
-    assertEquals(string, """{"event":"ping","body":{"sent":123,"age":1.0}}""")
-  }
+  test("PingEvent JSON"):
+    val string = PingEvent(123, 1.seconds).asJson
+    val expected = parse("""{"event":"ping","body":{"sent":123,"age":1.0}}""").toOption.get
+    assertEquals(string, expected)
+
+  test("Events json"):
+    val inp =
+      """
+        |{
+        |  "event": "coords",
+        |  "body": {
+        |    "coords": [
+        |      {
+        |        "depth": 0,
+        |        "id": 1507274,
+        |        "coord": {
+        |          "lng": 24.88320285,
+        |          "lat": 60.15414054
+        |        },
+        |        "boatTime": "27.10.2023 19:27:13",
+        |        "boatTimeMillis": 1698424033000,
+        |        "boatTimeOnly": "19:27:13",
+        |        "speed": 0.0,
+        |        "altitude": 46.000732421875,
+        |        "outsideTemp": 3.0,
+        |        "waterTemp": 0.0,
+        |        "depthMeters": 0.0,
+        |        "time": {
+        |          "date": "27.10.2023",
+        |          "time": "19:27:13",
+        |          "dateTime": "27.10.2023 19:27:13",
+        |          "millis": 1698424033000
+        |        }
+        |      }
+        |    ],
+        |    "from": {
+        |      "distance": 740647,
+        |      "track": 2794,
+        |      "trackName": "slyyxy",
+        |      "trackTitle": null,
+        |      "canonical": "slyyxy",
+        |      "comments": null,
+        |      "boat": 1319,
+        |      "boatName": "Mos",
+        |      "sourceType": "vehicle",
+        |      "username": "mle",
+        |      "points": 187,
+        |      "duration": 220.0,
+        |      "distanceMeters": 740.6478150355647,
+        |      "topSpeed": 23.28772354211663,
+        |      "avgSpeed": 8.39851862850972,
+        |      "avgWaterTemp": null,
+        |      "avgOutsideTemp": 2.304812834224599,
+        |      "topPoint": {
+        |        "depth": 0,
+        |        "id": 1507348,
+        |        "coord": {
+        |          "lng": 24.88176911,
+        |          "lat": 60.1532371
+        |        },
+        |        "boatTime": "27.10.2023 19:29:00",
+        |        "boatTimeMillis": 1698424140000,
+        |        "boatTimeOnly": "19:29:00",
+        |        "speed": 23.28772354211663,
+        |        "altitude": 36.74737548828125,
+        |        "outsideTemp": 2.0,
+        |        "waterTemp": 0.0,
+        |        "depthMeters": 0.0,
+        |        "time": {
+        |          "date": "27.10.2023",
+        |          "time": "19:29:00",
+        |          "dateTime": "27.10.2023 19:29:00",
+        |          "millis": 1698424140000
+        |        }
+        |      },
+        |      "times": {
+        |        "start": {
+        |          "date": "27.10.2023",
+        |          "time": "19:27:13",
+        |          "dateTime": "27.10.2023 19:27:13",
+        |          "millis": 1698424033000
+        |        },
+        |        "end": {
+        |          "date": "27.10.2023",
+        |          "time": "19:30:53",
+        |          "dateTime": "27.10.2023 19:30:53",
+        |          "millis": 1698424253000
+        |        },
+        |        "range": "27.10.2023 19:27:13 - 19:30:53"
+        |      }
+        |    }
+        |  }
+        |}
+        |""".stripMargin
+    val result = decode[CoordsEvent](inp)
+    assert(result.isRight)
