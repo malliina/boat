4e56ba4bc770d9f5a7e2c157b483d45c873b1c47
Michael Skogberg <malliina123@gmail.com>
2024-06-14 18:18:39 +0300

    Add stream example

---
+++ agent/src/test/scala/com/malliina/boat/client/TCPTests.scala
@@ -0,0 +1,21 @@
+package com.malliina.boat.client
+
+import cats.effect.IO
+import com.comcast.ip4s.{host, port}
+
+class TCPTests extends munit.CatsEffectSuite:
+  test("Stream live messages".ignore):
+    val host = host"192.168.77.11"
+    val port = port"10110"
+    for
+      client <- TcpClient.default[IO](host, port)
+      connection <- client.connect().compile.drain.start
+      msgs <- client.sentencesHub
+        .take(10)
+        .compile
+        .toList
+      _ <- client.close
+    yield
+      val messages = msgs.flatMap(_.sentences)
+      messages foreach println
+      assert(messages.nonEmpty)
