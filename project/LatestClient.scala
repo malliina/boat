94f568fc1ef95dcc61676bb69ef1e08decadc0fc
Michael Skogberg <malliina123@gmail.com>
2025-01-04 17:40:32 +0200

    Fix build recovery

--- project/LatestClient.scala
+++ project/LatestClient.scala
@@ -2,7 +2,6 @@ import LatestClient.Files
 import com.malliina.http.{FullUrl, OkClient}
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
-import sbt.internal.util.ManagedLogger
 import sbt.util.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -22,7 +21,14 @@ object LatestClient {
 class LatestClient(http: OkClient, log: Logger) {
   val url = FullUrl.https("www.boat-tracker.com", "/files")
 
-  def latest: Option[FullUrl] = Await.result(files, 10.seconds).latest
+  def latest: Option[FullUrl] =
+    try
+      Await.result(files, 10.seconds).latest
+    catch {
+      case e: Exception =>
+        log.error(s"Timed out fetching files from '$url'. $e")
+        None
+    }
 
   def files: Future[Files] = http.getAs[Files](url).recover { case t =>
     log.error(s"Failed to fetch files from '$url'. $t")
