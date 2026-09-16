c3d18349118b9604573126f95ba81bab8be27a4b
Michael Skogberg <malliina123@gmail.com>
2026-09-16 21:45:59 +0300

    Fix CI 2

--- backend/src/test/scala/com/malliina/boat/suites.scala
+++ backend/src/test/scala/com/malliina/boat/suites.scala
@@ -48,6 +48,7 @@ trait MUnitDatabaseSuite extends DoobieSQL:
       conf = testConf
         .parse[Password]("boat.db.pass")
         .map: pass =>
+          println(s"Using test pass '${pass.pass}'.")
           testDatabaseConf(pass)
 
     override def afterAll(): Unit = ()
